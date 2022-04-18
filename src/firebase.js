8fd0b3113452843423e87cb8596591dad2d7300c
TIJANI ABDELLATIF <tijani.idrissi.abdellatif@gmail.com>
2022-04-18 02:05:41 +0200

    setup

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBXbJcJVqlN2V4jqCu5RPFlZJS2iVkp7JQ",
+    authDomain: "netflix-clone-35b23.firebaseapp.com",
+    projectId: "netflix-clone-35b23",
+    storageBucket: "netflix-clone-35b23.appspot.com",
+    messagingSenderId: "201442280377",
+    appId: "1:201442280377:web:50a19282d3e39baffb2c68"
+  };
